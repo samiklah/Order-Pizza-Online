07a106106242f0c487f83876af9ac0bc4290c593
Sami Klah <samikla7@gmail.com>
2021-01-09 12:41:41 +0200

    the layout edited and bootstrab classes added

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -6,32 +6,51 @@
     <h1>Create a new pizza</h1>
     <form action="/pizzas" method="post">
         @csrf
-        <label for="name">Your name:</label>
-        <input type="text" id="" name="name">
-        <label for="type">Choose pizza type</label>
-        <select name="type" id="type">
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon3">Your name:</span>
+        </div>
+        <input type="text" name="name" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+        </div>
+        
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Choose pizza type</label>
+        </div>
+        <select class="custom-select" id="inputGroupSelect01">
+            <option selected>Choose...</option>
             <option value="cheese">Cheese</option>
             <option value="hawaiian">Hawaiian</option>
             <option value="veg supreme">veg supreme</option>
             <option value="volcano">volcano</option>
         </select>
+        </div>
 
-        <label for="base">Choose pizza base</label>
-        <select name="base" id="base">
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Choose pizza base</label>
+        </div>
+        <select class="custom-select" id="inputGroupSelect01">
+            <option selected>Choose...</option>
             <option value="cheese">Cheese</option>
             <option value="hawaiian">Hawaiian</option>
             <option value="veg supreme">veg supreme</option>
             <option value="volcano">volcano</option>
         </select>
+        </div>
+
+        <br>
+
         <fieldset>
-            <label for="">Extra toppings</label>
+            <label for="">Extra toppings</label><br>
             <input type="checkbox" name="toppings[]" id="" value="extra mashrooms"> Extra Cheese <br>
             <input type="checkbox" name="toppings[]" id="" value="peppers"> Peppers <br>
             <input type="checkbox" name="toppings[]" id="" value="Garlic"> Garlic <br>
             <input type="checkbox" name="toppings[]" id="" value="chilli"> Chilli <br>
             
-        </fieldset>
-        <input type="submit" value="Order pizza">
+        </fieldset> 
+        <br>
+        <input type="submit" class="btn btn-primary" value="Order pizza">
     </form>
 </div>
 @endsection
