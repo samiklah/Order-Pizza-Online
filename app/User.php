d244109b539b0c4761900f92f68ecfa2af241e3a
Sami Klah <samikla7@gmail.com>
2021-01-11 18:04:31 +0200

    C:/Program Files/Git/added a realationship user pizza

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pizza(){
+        return $this->hasMany('App\Pizza');
+    }
 }
